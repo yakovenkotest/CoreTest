64f866bc9f8def3308648127ddfda4c2fc2885be
Vadim Yakovenko <v.yakovenko@skgelios.ru>
2018-12-26 19:15:46 +0300

    Add simple event in Home Component

--- ClientApp/app/home/home.component.ts
+++ ClientApp/app/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    firstClick() {
+        console.log('clicked');
+    }
+
+    //(focus)="myMethod()"
+    //(blur)="myMethod()" 
+    //(submit)="myMethod()"  
+    //(scroll)="myMethod()"
+
+    //(cut)="myMethod()"
+    //(copy)="myMethod()"
+    //(paste)="myMethod()"
+
+    //(keydown)="myMethod()"
+    //(keypress)="myMethod()"
+    //(keyup)="myMethod()"
+
+    //(mouseenter)="myMethod()"
+    //(mousedown)="myMethod()"
+    //(mouseup)="myMethod()"
+
+    //(click)="myMethod()"
+    //(dblclick)="myMethod()"
+
+    //(drag)="myMethod()"
+    //(dragover)="myMethod()"
+    //(drop)="myMethod()"
 }
