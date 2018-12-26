ce45873c18cddb56bf2c3afbd3df9cf8357196b5
Vadim Yakovenko <v.yakovenko@skgelios.ru>
2018-12-26 18:15:26 +0300

    Add new Angular components (nav, about, contacts, home)

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
